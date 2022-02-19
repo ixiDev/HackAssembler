14e16204565ecbd3fa2ef7a3c3c612be0c0ade77
ixi <feedback.mrzero@gmail.com>
2022-02-19 13:59:10 +0100

    - Assembly file from main args

--- src/main/kotlin/com/ixidev/hackassembler/Main.kt
+++ src/main/kotlin/com/ixidev/hackassembler/Main.kt
@@ -1,10 +1,19 @@
 package com.ixidev.hackassembler
 
+import java.io.File
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
         println("Usage :  HackAssembler path/file.asm")
         exitProcess(1)
+    }
+    val program = File(args.first())
+    val assembler = Code(program)
+    try {
+        val hackPath = assembler.assembly()
+        println("Assembly successfully, hack file saved to '$hackPath'")
+    } catch (e: Exception) {
+        println("${e.message}")
     }
 }
