d66bbb2e5ba0c16423f9a441bb28d5fced836897
ixi <feedback.mrzero@gmail.com>
2022-02-19 14:10:09 +0100

    - Fix get files in ParserTest.kt

--- src/test/kotlin/com/ixidev/hackassembler/ParserTest.kt
+++ src/test/kotlin/com/ixidev/hackassembler/ParserTest.kt
@@ -16,7 +16,7 @@ internal class ParserTest {
     @Test
     fun test_parse_instructions_Add() {
         val parser = Parser(
-            File("/home/ixi/IdeaProjects/nand2tetris/HackAssembler/src/test/resources/Add.asm"),
+            File(javaClass.classLoader.getResource("Add.asm")!!.toURI()),
             SymbolTable()
         )
         val instructions = parser.parse()
@@ -27,7 +27,7 @@ internal class ParserTest {
     @Test
     fun test_parse_instructions_Max() {
         val parser = Parser(
-            File("/home/ixi/IdeaProjects/nand2tetris/HackAssembler/src/test/resources/Max.asm"),
+            File(javaClass.classLoader.getResource("Max.asm")!!.toURI()),
             SymbolTable()
         )
         val instructions = parser.parse()
